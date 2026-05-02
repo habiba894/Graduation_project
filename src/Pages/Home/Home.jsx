bfd6550bb05be5f0eba268af0b3095837b9714ee
EMAA06291 <emyyy212@gmail.com>
2026-05-02 20:56:57 +0300

    update

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,17 +1,36 @@
+import { useState, useEffect } from "react";
 import Navbar from "../../Components/Navbar";
 import Hero from "./Hero";
 import Destinations from "./Destinations";
-import Hotels from "./Hotels";
+import HotelsSection from "../../Pages/Country/HotelsSection";
 import CTA from "./CTA";
 import Footer from "./Footer";
 
 const Home = () => {
+  const countries = ["egypt", "france", "turkey"];
+  const [currentCountry, setCurrentCountry] = useState(countries[0]);
+
+  useEffect(() => {
+    let index = 0;
+
+    const interval = setInterval(() => {
+      index = (index + 1) % countries.length;
+      setCurrentCountry(countries[index]);
+    }, 4000); // كل 4 ثواني
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <Navbar />
       <Hero />
       <Destinations />
-      <Hotels />
+
+      <div className="transition-all duration-700 ease-in-out">
+        <HotelsSection countryName={currentCountry} />
+      </div>
+
       <CTA />
       <Footer />
     </>
